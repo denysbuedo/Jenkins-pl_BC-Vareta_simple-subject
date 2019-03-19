d86e24c8877e6679cea7841c9d5fc32b0376b765
denysbuedo <45684658+denysbuedo@users.noreply.github.com>
2019-03-19 18:13:03 +0800

    Add clear stage

--- jenkins.sh
+++ jenkins.sh
@@ -17,8 +17,9 @@ if [[ $1 = 'run' ]];
            mkdir /data3_260T/DATA/Datasets/Data_Proccesaded/BC-Vareta/$2
            mv /home/software_install_dir/MATLAB/R2018a/$7.tar.gz /data3_260T/DATA/Datasets/Data_Proccesaded/BC-Vareta/$2
        fi
-      cd /home/software_install_dir/MATLAB/R2018a/
-      rm -rf /home/software_install_dir/MATLAB/R2018a/$7/
-    else
+  elif [[ $1 = 'clean' ]];
+    then
+      rm -r /home/software_install_dir/MATLAB/R2018a/$7
+  else
       echo "Invalid action"
   fi
